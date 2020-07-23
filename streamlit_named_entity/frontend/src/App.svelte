35226760ffb4ca0e3e088a59637cf0b814d5ce2a
Fanilo Andrianasolo <fanilo.andrianasolo@worldline.com>
2020-07-23 15:19:16 +0200

    Create Entity component

--- streamlit_named_entity/frontend/src/App.svelte
+++ streamlit_named_entity/frontend/src/App.svelte
@@ -1,30 +1,29 @@
 <script lang="ts">
-	export let name: string;
+  import Entity from "./components/Entity.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  section {
+    padding: 1em;
+  }
+  header {
+    background-color: rgb(88, 63, 207);
+    color: #eee;
+    padding: 0.1em 1em;
+    margin-bottom: 1em;
+  }
+  main {
+    padding: 1em;
+  }
 </style>
+
+<section>
+  <header>
+    <h1>Named entity demo</h1>
+  </header>
+  <main>
+    Hello
+    <Entity word="Larry page" entity="PRODUCT" />
+    Hello again
+  </main>
+</section>
---
+++ streamlit_named_entity/frontend/src/components/Entity.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+  export let word: string;
+  export let entity: string;
+
+  const colors = {
+    ORG: "#7aecec",
+    PRODUCT: "#bfeeb7",
+    GPE: "#feca74",
+    LOC: "#ff9561",
+    PERSON: "#aa9cfc",
+    NORP: "#c887fb",
+    FACILITY: "#9cc9cc",
+    EVENT: "#ffeb80",
+    LAW: "#ff8197",
+    LANGUAGE: "#ff8197",
+    WORK_OF_ART: "#f0d0ff",
+    DATE: "#bfe1d9",
+    TIME: "#bfe1d9",
+    MONEY: "#e4e7d2",
+    QUANTITY: "#e4e7d2",
+    ORDINAL: "#e4e7d2",
+    CARDINAL: "#e4e7d2",
+    PERCENT: "#e4e7d2",
+  };
+  const entityColor = `background-color: ${colors[entity] || "#ddd"}`;
+</script>
+
+<style>
+  mark {
+    background-color: lightblue;
+    padding: 0.45em 0.6em;
+    margin: 0 0.25em;
+    line-height: 1;
+    border-radius: 0.35em;
+  }
+
+  .entity {
+    font-size: 0.8em;
+    font-weight: bold;
+    line-height: 1;
+    text-transform: uppercase;
+    vertical-align: middle;
+    margin-left: 0.5rem;
+  }
+</style>
+
+<mark style={entityColor}>
+  {word}
+  <span class="entity">{entity}</span>
+</mark>
